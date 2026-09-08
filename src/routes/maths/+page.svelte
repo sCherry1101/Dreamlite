36f010a777d8d668a3ed519e1510aae723994061
sCherry1101 <cherry.1101.8113@gmail.com>
2026-09-08 16:36:05 +0530

    build: math module page

--- src/routes/maths/+page.svelte
+++ src/routes/maths/+page.svelte
@@ -4,6 +4,16 @@
 
 <div id="maths_box">
   <h1>Choose your topic below</h1>
-  <Button href="/maths/pythagoras" variant="primary">Pythagoras Theorem</Button>
-  <Button href="/mensuration" variant="primary">Mensuration</Button>
+
+  <div class="card-group">
+    <div class="subject-card">
+      <h2>Pythagoras Theorem</h2>
+     <Button href="/maths/pythagoras" variant="primary">Click here</Button>
+  </div>
+
+    <div class="subject-card">
+      <h2>Mensuration</h2>
+     <Button href="/mensuration" variant="primary">Click here</Button>
+    </div>
+  </div>
 </div>
