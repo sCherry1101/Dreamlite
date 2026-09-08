cc8324ff821dec932b8c396c2fa6aa8f0784348e
sCherry1101 <cherry.1101.8113@gmail.com>
2026-09-08 16:24:52 +0530

    add: pythagoras and mensuration module

--- src/routes/maths/+page.svelte
+++ src/routes/maths/+page.svelte
@@ -1,3 +1,9 @@
-<div id="error_div">
+<script>
+  import Button from "$component/Button"
+</script>
+
+<div id="maths_box">
   <h1>Choose your topic below</h1>
+  <Button href="/maths/pythagoras" variant="primary">Pythagoras Theorem</Button>
+  <Button href="/mensuration" variant="primary">Mensuration</Button>
 </div>
